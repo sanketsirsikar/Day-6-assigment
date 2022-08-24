0413a26a4c4d185dfe22133bf54ef82dc4430f17
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-08-24 15:09:03 +0530

    Fibonacci Series

---
+++ Day-6 assigment/src/logicalprogram/FibonacciSeries.java
@@ -0,0 +1,16 @@
+package logicalprogram;
+
+public class FibonacciSeries {
+	public static void main(String[] args) {
+		int a=0, b=1;
+		System.out.println(a + " " +b);
+		int c;
+		
+		for (int i=1; i<=10; i++) {
+			c= a+b;
+			System.out.println("" + c);
+			a=b;
+			b=c;
+		}
+}
+}

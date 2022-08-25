9429db4d7b552f0185c21f979ce542efb68e6221
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-08-25 21:39:29 +0530

    Reverse number

---
+++ Day-6 assigment/src/logicalprogram/Reversenumber.java
@@ -0,0 +1,13 @@
+package logicalprogram;
+
+public class Reversenumber {
+    public static void main(String[] args) {
+      int no =5962,rem, rev=0;
+      while (no !=0) {
+    	  rem = no %10;
+    	  rev = rev*10+rem;
+    	  no=no/10;
+      }
+      System.out.println(rev);
+}
+}
